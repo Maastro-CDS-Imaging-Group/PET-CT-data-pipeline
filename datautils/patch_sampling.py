e346dfbc69bb35fd89d85557e961bf988a7cfecd
cnmy-ro <chinmay.srao97@gmail.com>
2020-10-20 15:04:23 +0200

    Added focal point stride option in samplers to enable overlap adjustment

--- datautils/patch_sampling.py
+++ datautils/patch_sampling.py
@@ -12,10 +12,11 @@ class PatchSampler3D():
     """
     Samples 3D patches of specified size using the specified sampling method.
     """
-    def __init__(self, patch_size, sampling='random'):
+    def __init__(self, patch_size, sampling='random', focal_point_stride=1):
         self.patch_size = list(patch_size) # Specified in (W,H,D) order
         self.patch_size.reverse()    # Convert to (D,H,W) order
         self.sampling = sampling # TODO - Add new schemes
+        self.focal_point_stride = focal_point_stride # Useful while using sequential sampling
 
 
     def get_samples(self, subject_dict, num_patches):
@@ -61,9 +62,9 @@ class PatchSampler3D():
             xs = np.random.randint(valid_indx_range[0][2], valid_indx_range[1][2], num_patches)
         elif self.sampling == 'sequential':
             # arange takes exlusive range
-            z_range = np.arange(valid_indx_range[0][0], valid_indx_range[1][0] + 1).astype(np.int)
-            y_range = np.arange(valid_indx_range[0][1], valid_indx_range[1][1] + 1).astype(np.int)
-            x_range = np.arange(valid_indx_range[0][2], valid_indx_range[1][2] + 1).astype(np.int)
+            z_range = np.arange(valid_indx_range[0][0], valid_indx_range[1][0] + 1, self.focal_point_stride).astype(np.int)
+            y_range = np.arange(valid_indx_range[0][1], valid_indx_range[1][1] + 1, self.focal_point_stride).astype(np.int)
+            x_range = np.arange(valid_indx_range[0][2], valid_indx_range[1][2] + 1, self.focal_point_stride).astype(np.int)
             zs, ys, xs = np.meshgrid(z_range, y_range, x_range, indexing='ij')
             zs, ys, xs = zs.flatten(), ys.flatten(), xs.flatten()
 
@@ -75,10 +76,12 @@ class PatchSampler2D():
     """
     Samples 2D axial slice patches of specified x-y size using the specified sampling method.
     """
-    def __init__(self, patch_size, sampling='random'):
+    def __init__(self, patch_size, sampling='random', focal_point_stride=1):
         self.patch_size = list(patch_size) # Specified in (W,H) order
         self.patch_size.reverse()    # Convert to (H,W) order
         self.sampling = sampling # TODO
+        self.focal_point_stride = focal_point_stride # Useful while using sequential sampling
+
 
 
     def get_samples(self, subject_dict, num_patches):
@@ -124,9 +127,9 @@ class PatchSampler2D():
             xs = np.random.randint(valid_indx_range[0][1], valid_indx_range[1][1], num_patches)
         elif self.sampling == 'sequential':
             # arange takes exclusive range
-            z_range = np.arange(0, volume_shape[0]).astype(np.int)
-            y_range = np.arange(valid_indx_range[0][0], valid_indx_range[1][0] + 1).astype(np.int)
-            x_range = np.arange(valid_indx_range[0][1], valid_indx_range[1][1] + 1).astype(np.int)
+            z_range = np.arange(0, volume_shape[0], self.focal_point_stride).astype(np.int)
+            y_range = np.arange(valid_indx_range[0][0], valid_indx_range[1][0] + 1, self.focal_point_stride).astype(np.int)
+            x_range = np.arange(valid_indx_range[0][1], valid_indx_range[1][1] + 1, self.focal_point_stride).astype(np.int)
             zs, ys, xs = np.meshgrid(z_range, y_range, x_range, indexing='ij')
             zs, ys, xs = zs.flatten(), ys.flatten(), xs.flatten()
 
@@ -210,7 +213,7 @@ class PatchQueue(Dataset):
 
 
 
-def get_num_valid_patches(patch_size, volume_size=(450,450,100)):
+def get_num_valid_patches(patch_size, volume_size=(450,450,100), focal_point_stride=1):
     dimensions = len(patch_size)
     num_slices = volume_size[2]
     if dimensions == 2 and len(volume_size) == 3:
@@ -224,8 +227,15 @@ def get_num_valid_patches(patch_size, volume_size=(450,450,100)):
                        ]
 
     valid_area_size = valid_indx_range[1] - valid_indx_range[0] + 1
-    if dimensions == 2:   num_valid_focal_pts = valid_area_size[0] * valid_area_size[1] * num_slices
-    elif dimensions == 3:   num_valid_focal_pts = valid_area_size[0] * valid_area_size[1] * valid_area_size[2]
+    if dimensions == 2:
+        num_valid_focal_pts = np.ceil(float(valid_area_size[0]/focal_point_stride)) * \
+                              np.ceil(float(valid_area_size[1]/focal_point_stride)) * \
+                              np.ceil(float(num_slices/focal_point_stride))
+    elif dimensions == 3:
+        num_valid_focal_pts = np.ceil(float(valid_area_size[0]/focal_point_stride)) * \
+                              np.ceil(float(valid_area_size[1]/focal_point_stride)) * \
+                              np.ceil(float(valid_area_size[2]/focal_point_stride))
+
     return int(num_valid_focal_pts)
 
 
@@ -338,5 +348,31 @@ if __name__ == '__main__':
         print("Sample patch shape:", sample_patch['PET-CT'].shape, sample_patch['GTV-labelmap'].shape)
 
 
+    def test_focal_point_stride(focal_point_stride=3):
 
-    test_dataloader_limit()
+        PET_dataset = HECKTORUnimodalDataset(data_dir,
+                                    patient_id_filepath,
+                                    mode='cval-CHMR-validation',
+                                    preprocessor=preprocessor,
+                                    input_modality='PET',
+                                    augment_data=False)
+        subject_dict = PET_dataset[0]
+
+        print("focal_point_stride:", focal_point_stride)
+
+        print("Testing 3D ...")
+        patch_size_3d = (448,448,98)
+        patch_sampler_3d = PatchSampler3D(patch_size=patch_size_3d, sampling='sequential', focal_point_stride=focal_point_stride)
+        num_valid_patches = get_num_valid_patches(patch_size_3d, focal_point_stride=focal_point_stride)
+        focal_pts = patch_sampler_3d._sample_valid_focal_points(subject_dict, num_valid_patches)
+        print("Focal pts:", focal_pts)
+
+        print("Testing 2D ...")
+        patch_size_2d = (448,448)
+        patch_sampler_2d = PatchSampler2D(patch_size=patch_size_2d, sampling='sequential', focal_point_stride=focal_point_stride)
+        num_valid_patches = get_num_valid_patches(patch_size_2d, focal_point_stride=focal_point_stride)
+        focal_pts = patch_sampler_2d._sample_valid_focal_points(subject_dict, num_valid_patches)
+        #print("Focal pts:", focal_pts)
+
+
+    test_focal_point_stride(focal_point_stride=3)
