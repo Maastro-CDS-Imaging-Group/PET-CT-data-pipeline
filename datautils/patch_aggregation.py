e42a7590590def8ccc938428fb52ca04cc217f5e
cnmy-ro <chinmay.srao97@gmail.com>
2020-10-22 23:09:06 +0200

    Updated aggregator, added sampling testing scripts

--- datautils/patch_aggregation.py
+++ datautils/patch_aggregation.py
@@ -5,12 +5,18 @@ class PatchAggregator3D():
     def __init__(self, patch_size=(128,128,32), volume_size=(144,144,48), focal_point_stride=(5,5,5), overlap_handling=None):
 
         """
-        All coordinates and shapes are in (W,H,D) format
+        All arguments are apecified in (W,H,D) format
         """
 
-        self.patch_size = patch_size
-        self.volume_size = volume_size
-        self.focal_point_stride = focal_point_stride
+        self.patch_size = list(patch_size)
+        self.volume_size = list(volume_size)
+        self.focal_point_stride = list(focal_point_stride)
+
+        # Convert to (D,H,W) ordering
+        self.patch_size.reverse()
+        self.volume_size.reverse()
+        self.focal_point_stride.reverse()
+
         self.overlap_handling = overlap_handling  # None or 'union'
 
         self.valid_focal_points = self._get_valid_focal_points() # Valid focal points in volume coordinates
@@ -34,6 +40,12 @@ class PatchAggregator3D():
 
 
     def aggregate(self, patches_list):
+        """
+        Args:
+            patches_list: List of numpy arrays. Shape of the each array is in (D,H,W) ordering
+        Returns:
+        full_volume: Aggregated from the patches. shape has dim (D,H,W) ordering
+        """
         # Define a zeros array of shape volume_size
         full_volume = np.zeros(self.volume_size)
 
@@ -44,31 +56,63 @@ class PatchAggregator3D():
             # Find the indices of the volume where the patch needs to be placed
             global_focal_point = np.array(self.valid_focal_points[i])
             global_start_idxs = global_focal_point.astype(np.int) - np.floor(patch_size/2).astype(np.int)
-            x1, y1, z1 = global_start_idxs
-            x2, y2, z2 = global_start_idxs + patch_size
+            z1, y1, x1 = global_start_idxs
+            z2, y2, x2 = global_start_idxs + patch_size
 
             # Handle overlap or not
             if self.overlap_handling is None:
-                full_volume[x1:x2, y1:y2, z1:z2] = patch
+                full_volume[z1:z2, y1:y2, x1:x2] = patch
 
             if self.overlap_handling == 'union':
                 full_volume_copy = full_volume.copy()
-                full_volume[x1:x2, y1:y2, z1:z2] = patch
+                full_volume[z1:z2, y1:y2, x1:x2] = patch
                 full_volume = np.maximum(full_volume, full_volume_copy)
+
+            if self.overlap_handling == 'intersection':
+                full_volume_copy = full_volume.copy()
+                full_volume[z1:z2, y1:y2, x1:x2] = patch
+                full_volume = full_volume * full_volume_copy
 
         return full_volume
 
 
 
+def get_pred_labelmap_patches_list(pred_prob_patches):
+    """
+    Get a list of predicted labelmap patches from the the model's predicted batch of probabilities patches
+
+    Args:
+        pred_prob_patches: Tensor. Batch of predicted probabilites patches. Shape (N,C,D,H,W)
+    Returns:
+        pred_labelmap_patches_list: List of length N. Each element is a tensor of hape (D,H,W)
+    """
+    pred_labelmap_patches_list = []
+
+    for i in range(pred_prob_patches.shape[0]):
+
+        # Convert to numpy, collapse channel dim for the predicted patch
+        pred_patch = pred_prob_patches[i].numpy() # Shape (C,D,H,W)
+        pred_patch = np.argmax(pred_patch, axis=0)  # Shape (D,H,W)
+
+        # Accumulate in the list
+        pred_labelmap_patches_list.append(pred_patch)
+
+    return pred_labelmap_patches_list
+
+
+
 if __name__ == '__main__':
+
+    # Arguments specified in (W,H,D) ordering
     volume_size = (144,144,48)
     patch_size = (128,128,32)
     focal_point_stride = (10,10,10)
     patch_aggregator = PatchAggregator3D(patch_size,
-                                     volume_size,
-                                     focal_point_stride,
-                                     overlap_handling='union')
+                                        volume_size,
+                                        focal_point_stride,
+                                        overlap_handling=None)
 
-    patches_list = [np.ones(patch_size) for i in range(8)]
+    # Patches have (D,H,W) order internally
+    patches_list = [np.zeros(patch_size[2], patch_size[1], patch_size[0]) for i in range(8)]
 
     recovered_labelmap = patch_aggregator.aggregate(patches_list)
