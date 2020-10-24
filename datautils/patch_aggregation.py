2f644cd816413b592aa1daa302980f8a8ec06f37
cnmy-ro <chinmay.srao97@gmail.com>
2020-10-25 01:06:39 +0200

    Added strided-random sampling for training; found and fixed aggregator related issue

--- datautils/patch_aggregation.py
+++ datautils/patch_aggregation.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class PatchAggregator3D():
-    def __init__(self, patch_size=(128,128,32), volume_size=(144,144,48), focal_point_stride=(5,5,5), overlap_handling=None):
+    def __init__(self, patch_size=(128,128,32), volume_size=(144,144,48), focal_point_stride=(5,5,5), overlap_handling=None, unpadding=0):
 
         """
         All arguments are apecified in (W,H,D) format
@@ -18,6 +18,11 @@ class PatchAggregator3D():
         self.focal_point_stride.reverse()
 
         self.overlap_handling = overlap_handling  # None or 'union'
+        self.unpadding = unpadding
+
+        # Increase the volume size to account for the padding used during patch sampling
+        if unpadding > 0:
+            self.volume_size = [s + unpadding for s in self.volume_size] # (D,H,W)
 
         self.valid_focal_points = self._get_valid_focal_points() # Valid focal points in volume coordinates
 
@@ -44,7 +49,7 @@ class PatchAggregator3D():
         Args:
             patches_list: List of numpy arrays. Shape of the each array is in (D,H,W) ordering
         Returns:
-        full_volume: Aggregated from the patches. shape has dim (D,H,W) ordering
+            full_volume: Aggregated from the patches. shape has dim (D,H,W) ordering
         """
         # Define a zeros array of shape volume_size
         full_volume = np.zeros(self.volume_size)
@@ -63,15 +68,14 @@ class PatchAggregator3D():
             if self.overlap_handling is None:
                 full_volume[z1:z2, y1:y2, x1:x2] = patch
 
-            if self.overlap_handling == 'union':
+            elif self.overlap_handling == 'union':
                 full_volume_copy = full_volume.copy()
                 full_volume[z1:z2, y1:y2, x1:x2] = patch
                 full_volume = np.maximum(full_volume, full_volume_copy)
 
-            if self.overlap_handling == 'intersection':
-                full_volume_copy = full_volume.copy()
-                full_volume[z1:z2, y1:y2, x1:x2] = patch
-                full_volume = full_volume * full_volume_copy
+        # If padding was used during patch sampling, remove it from the full volume
+        if self.unpadding > 0:
+            full_volume = full_volume[:-self.unpadding, :-self.unpadding, :-self.unpadding]
 
         return full_volume
 
@@ -103,16 +107,45 @@ def get_pred_labelmap_patches_list(pred_prob_patches):
 
 if __name__ == '__main__':
 
+    import torch
+    from patch_sampling import PatchSampler3D, get_num_valid_patches
+
+    np.random.seed(0)
+
     # Arguments specified in (W,H,D) ordering
     volume_size = (144,144,48)
     patch_size = (128,128,32)
-    focal_point_stride = (10,10,10)
+    focal_point_stride = (20,20,20)
+    padding = 4
+
+    patch_sampler = PatchSampler3D(patch_size,
+                                   sampling='sequential',
+                                   focal_point_stride=focal_point_stride,
+                                   padding=padding)
+
     patch_aggregator = PatchAggregator3D(patch_size,
                                         volume_size,
                                         focal_point_stride,
-                                        overlap_handling=None)
+                                        overlap_handling=None,
+                                        unpadding=padding)
 
-    # Patches have (D,H,W) order internally
-    patches_list = [np.zeros(patch_size[2], patch_size[1], patch_size[0]) for i in range(8)]
+    num_valid_patches = get_num_valid_patches(patch_size, volume_size, focal_point_stride, padding=padding)
+    print("Num patches:", num_valid_patches)
+
+    # (D,H,W) order followed internally
+    random_labelmap = np.random.randint(low=0, high=2, size=(volume_size[2], volume_size[1], volume_size[0]))
+
+    patches_list = patch_sampler.get_samples(subject_dict={'GTV-labelmap': torch.from_numpy(random_labelmap)},
+                                    num_patches=num_valid_patches)
+
+    patches_list = [patch['GTV-labelmap'] for patch in patches_list]
 
     recovered_labelmap = patch_aggregator.aggregate(patches_list)
+
+
+    def dice(labelmap_1, labelmap_2):
+        intersection = np.sum(labelmap_1 * labelmap_2)
+        dice_score = 2 * intersection / (np.sum(labelmap_1) + np.sum(labelmap_2))
+        return dice_score
+
+    print(dice(random_labelmap, recovered_labelmap))
